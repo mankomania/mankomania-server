a6c365a83c3bf664ab420fec860b7818dbb7cf58
Valentina Schiavon <valentinaschiavon99@gmail.com>
2025-04-06 23:40:45 +0200

    feat: add HorseRaceController with /horse-race/run endpoint

--- src/main/kotlin/at/mankomania/server/controller/HorseRaceController.kt
+++ src/main/kotlin/at/mankomania/server/controller/HorseRaceController.kt
@@ -1,4 +1,23 @@
-package at.mankomania.server
+package at.mankomania.server.controller
 
-class HorseRaceController {
+import at.mankomania.server.model.Bet
+import at.mankomania.server.service.HorseRaceService
+import org.springframework.web.bind.annotation.*
+
+@RestController
+@RequestMapping("/horse-race")
+class HorseRaceController(
+    private val horseRaceService: HorseRaceService
+) {
+
+    @PostMapping("/run")
+    fun runRace(@RequestBody bets: List<Bet>): Map<String, Any> {
+        val winner = horseRaceService.runRace()
+        val winnings = horseRaceService.calculateWinnings(bets, winner)
+
+        return mapOf(
+            "winner" to winner,
+            "winnings" to winnings
+        )
+    }
 }
