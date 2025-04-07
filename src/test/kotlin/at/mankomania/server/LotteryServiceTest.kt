c5b23dbfd6d8ea7df686bece5c7baa6229b4873c
vanidrei <vanfroehlich@edu.aau.at>
2025-04-07 15:35:59 +0200

    Add basic setup for LotteryService unit tests
    - Create LotteryServiceTest class
    - Add setup method to initialize LotteryService and different Player instances (rich, poor, bankrupt)

---
+++ src/test/kotlin/at/mankomania/server/LotteryServiceTest.kt
@@ -0,0 +1,22 @@
+package at.mankomania.server
+
+import at.mankomania.server.model.Player
+import at.mankomania.server.service.LotteryService
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Test
+import kotlin.test.*
+
+class LotteryServiceTest {
+    private lateinit var lotteryService: LotteryService
+    private lateinit var richPlayer: Player
+    private lateinit var poorPlayer: Player
+    private lateinit var bankruptPlayer: Player
+
+    @BeforeEach
+    fun setup() {
+        lotteryService = LotteryService()
+        richPlayer = Player("rich", 100000)
+        poorPlayer = Player("poor", 4000)
+        bankruptPlayer = Player("bankrupt", 0)
+    }
+}
