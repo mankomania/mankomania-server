bbdd3b38eb83f6c53a9c4f6e81ba9e9aa19b11f3
MauMauChat <lucaan@edu.aau.at>
2025-04-07 21:38:59 +0200

    Add WebSocketManager: maintains mapping of session IDs to sessions

--- src/main/kotlin/at/mankomania/server/websocket/WebSocketManager.kt
+++ src/main/kotlin/at/mankomania/server/websocket/WebSocketManager.kt
@@ -1,4 +1,26 @@
 package at.mankomania.server.websocket
 
-class WebSocketManager {
+import org.springframework.web.socket.WebSocketSession
+
+/**
+ * WebSocketManager manages active WebSocket sessions.
+ *
+ * It maintains a mapping from session ID to WebSocketSession.
+ */
+object WebSocketManager {
+    private val sessions = mutableMapOf<String, WebSocketSession>()
+
+    fun addSession(session: WebSocketSession) {
+        sessions[session.id] = session
+    }
+
+    fun removeSession(session: WebSocketSession) {
+        sessions.remove(session.id)
+    }
+
+    fun getSession(id: String): WebSocketSession? = sessions[id]
+
+    fun clearSessions() {
+        sessions.clear()
+    }
 }
