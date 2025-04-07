160295d2529e79ae04ed372d731995def7701334
Angie Drucks <angie.drucks@gmail.com>
2025-04-07 19:49:19 +0200

    feat: Add CellState enum for board cell states
    - Added CellState enum with two states: FREE and OCCUPIED.
    - This enum is used to represent the state of a board cell in the game. #9

---
+++ src/main/kotlin/at/mankomania/server/model/CellState.kt
@@ -0,0 +1,17 @@
+/**
+ * @file CellState.kt
+ * @author Angela Drucks
+ * @since 2025-04-02
+ * @description
+ */
+
+package at.mankomania.server.model
+
+/**
+ * Represents the state of a board cell.
+ */
+
+enum class CellState {
+    FREE,
+    OCCUPIED
+}
