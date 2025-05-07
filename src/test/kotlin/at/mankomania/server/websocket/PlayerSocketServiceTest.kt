b8c02683d03ef708590c2b8d28fd2b551b543eb0
Valentina Schiavon <valentinaschiavon99@gmail.com>
2025-05-07 11:46:48 +0200

    test: add edge case tests for PlayerSocketService (special characters and large money map)

--- src/test/kotlin/at/mankomania/server/websocket/PlayerSocketServiceTest.kt
+++ src/test/kotlin/at/mankomania/server/websocket/PlayerSocketServiceTest.kt
@@ -1,5 +1,6 @@
+package at.mankomania.server.websocket
+
 import at.mankomania.server.model.Player
-import at.mankomania.server.websocket.PlayerSocketService
 import junit.framework.TestCase.assertEquals
 import org.junit.Test
 import org.springframework.messaging.Message
@@ -8,7 +9,6 @@ import org.springframework.messaging.simp.SimpMessagingTemplate
 
 class PlayerSocketServiceTest {
 
-    // Dummy/fake implementation of SimpMessagingTemplate
     inner class DummyMessagingTemplate : SimpMessagingTemplate(StubMessageChannel()) {
         var lastDestination: String? = null
         var lastPayload: Any? = null
@@ -19,7 +19,6 @@ class PlayerSocketServiceTest {
         }
     }
 
-    // Simple stub implementation of MessageChannel
     private class StubMessageChannel : MessageChannel {
         override fun send(message: Message<*>): Boolean = true
         override fun send(message: Message<*>, timeout: Long): Boolean = true
@@ -27,19 +26,55 @@ class PlayerSocketServiceTest {
 
     @Test
     fun sendFinancialState_should_send_correct_topic_and_money() {
-        // Arrange
         val dummyTemplate = DummyMessagingTemplate()
         val service = PlayerSocketService.PlayerSocketService(dummyTemplate)
-        val player = Player(
-            name = "Player1",
-            money = mutableMapOf(5000 to 10)
-        )
+        val player = Player(name = "Player1", money = mutableMapOf(5000 to 10))
 
-        // Act
         service.sendFinancialState(player)
 
-        // Assert
         assertEquals("/topic/player/Player1/money", dummyTemplate.lastDestination)
         assertEquals(player.money, dummyTemplate.lastPayload)
     }
+
+    @Test
+    fun sendFinancialState_should_handle_empty_money() {
+        val dummyTemplate = DummyMessagingTemplate()
+        val service = PlayerSocketService.PlayerSocketService(dummyTemplate)
+        val player = Player(name = "NoMoneyPlayer", money = mutableMapOf()) //
+
+        service.sendFinancialState(player)
+
+        assertEquals("/topic/player/NoMoneyPlayer/money", dummyTemplate.lastDestination)
+        assertEquals(player.money, dummyTemplate.lastPayload)
+    }
+    @Test
+    fun sendFinancialState_should_handle_large_money_map() {
+        val dummyTemplate = DummyMessagingTemplate()
+        val service = PlayerSocketService.PlayerSocketService(dummyTemplate)
+        val moneyMap = mutableMapOf(
+            5000 to 100,
+            10000 to 50,
+            50000 to 20,
+            100000 to 10
+        )
+        val player = Player(name = "RichieRich", money = moneyMap)
+
+        service.sendFinancialState(player)
+
+        assertEquals("/topic/player/RichieRich/money", dummyTemplate.lastDestination)
+        assertEquals(moneyMap, dummyTemplate.lastPayload)
+    }
+
+    @Test
+    fun sendFinancialState_should_handle_empty_player_name() {
+        val dummyTemplate = DummyMessagingTemplate()
+        val service = PlayerSocketService.PlayerSocketService(dummyTemplate)
+        val player = Player(name = "", money = mutableMapOf(10000 to 2))
+
+        service.sendFinancialState(player)
+
+        assertEquals("/topic/player//money", dummyTemplate.lastDestination)
+        assertEquals(player.money, dummyTemplate.lastPayload)
+    }
 }
+
