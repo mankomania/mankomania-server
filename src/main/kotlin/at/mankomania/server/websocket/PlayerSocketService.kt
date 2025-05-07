4255554b947af646c56c4307973edb39804cf4d3
Valentina Schiavon <valentinaschiavon99@gmail.com>
2025-05-07 17:41:35 +0200

    fix: handle nullable money map in PlayerSocketService

--- src/main/kotlin/at/mankomania/server/websocket/PlayerSocketService.kt
+++ src/main/kotlin/at/mankomania/server/websocket/PlayerSocketService.kt
@@ -1,4 +1,13 @@
 package at.mankomania.server.websocket
 
-class PlayerSocketService {
+import org.springframework.messaging.simp.SimpMessagingTemplate
+import at.mankomania.server.model.Player
+
+class PlayerSocketService(private val messagingTemplate: SimpMessagingTemplate) {
+
+    fun sendFinancialState(player: Player) {
+        val destination = "/topic/player/${player.name}/money"
+        val moneyMap = player.money ?: mutableMapOf()
+        messagingTemplate.convertAndSend(destination, moneyMap)
+    }
 }
