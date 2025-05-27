f86b9252c3f4444714d56de54464decccb99f175
Angie Drucks <angie.drucks@gmail.com>
2025-05-27 18:39:54 +0200

    gamestart(server): Add parameter firstPlayerIndex into GameStartedDto.kt, because same as client

--- src/main/kotlin/at/mankomania/server/controller/dto/GameStartedDto.kt
+++ src/main/kotlin/at/mankomania/server/controller/dto/GameStartedDto.kt
@@ -7,10 +7,12 @@
  *
  * @property gameId The identifier of the started game.
  * @property startPositions The list of starting field indices for each player in join order.
+ * @property firstPlayerIndex Index (0-based) of the player who begins
  */
 package at.mankomania.server.controller.dto
 
 data class GameStartedDto(
     val gameId: String,
-    val startPositions: List<Int>
+    val startPositions: List<Int>,
+    val firstPlayerIndex: Int
 )
