85a60affe75308585a7911177cbd7d26db98561c
Anna Walder <annapalomawa@edu.aau.at>
2025-06-24 12:34:19 +0200

    remuve unused import to get rid of Sonar issue

--- src/main/kotlin/at/mankomania/server/controller/dto/GameStateDto.kt
+++ src/main/kotlin/at/mankomania/server/controller/dto/GameStateDto.kt
@@ -1,7 +1,6 @@
 package at.mankomania.server.controller.dto
 
 import at.mankomania.server.model.BoardCell
-import at.mankomania.server.model.Player
 
 data class GameStateDto(
     val players: List<PlayerDto>,
